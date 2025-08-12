4914252ec8f02e052dba6c2461fd843cfe31bdc4
lisamayo <lisamayo@users.noreply.github.com>
2025-08-12 15:26:12 -0400

    Create block-onetrust.js

---
+++ block-onetrust.js
@@ -0,0 +1,5 @@
+(function() {
+  const style = document.createElement('style');
+  style.innerHTML = '.ot-sdk-container, .ot-sdk-row { display:none !important; }';
+  document.documentElement.appendChild(style);
+})();
