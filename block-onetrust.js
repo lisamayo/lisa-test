6085b8f795b5061d1adfdb671193b87514fa9010
lisamayo <lisamayo@users.noreply.github.com>
2025-08-12 15:30:14 -0400

    Update block-onetrust.js

--- block-onetrust.js
+++ block-onetrust.js
@@ -1,5 +1,27 @@
-(function() {
-  const style = document.createElement('style');
-  style.innerHTML = '.ot-sdk-container, .ot-sdk-row { display:none !important; }';
-  document.documentElement.appendChild(style);
-})();
+  // Intercept creation of script tags and block those loading OneTrust scripts
+  (function() {
+    const originalCreateElement = document.createElement.bind(document);
+
+    document.createElement = function(tagName) {
+      const element = originalCreateElement(tagName);
+      if (tagName.toLowerCase() === 'script') {
+        Object.defineProperty(element, 'src', {
+          set: function(src) {
+            if (src && src.includes('cdn.cookielaw.org')) {
+              // Prevent loading OneTrust script by not setting src
+              console.log('Blocked OneTrust script:', src);
+              return;
+            }
+            // Otherwise set src normally
+            this.setAttribute('src', src);
+          },
+          get: function() {
+            return this.getAttribute('src');
+          },
+          configurable: true,
+          enumerable: true,
+        });
+      }
+      return element;
+    };
+  })();
